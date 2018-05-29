e4bbe9370f1fbf880204dada40163dd03eb68679
Balázs Vajner <vajner.balazs@gmail.com>
2018-05-29 08:45:43 +0200

    Added persistence for Klass

--- src/main/java/com/codecool/jpaexample/model/Klass.java
+++ src/main/java/com/codecool/jpaexample/model/Klass.java
@@ -4,8 +4,16 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity(name = "Class")
 public class Klass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String name;
+
+    @OneToMany
     private Set<Student> students = new HashSet<>();
 
     public Klass() {}
@@ -30,4 +38,7 @@ public class Klass {
         students.add(student);
     }
 
+    public int getId() {
+        return id;
+    }
 }
